8fc2fc6b914f1217c98a44b471377032c6ecb106
eriksmedeiros <erik.medeiros.701@ufrn.edu.br>
2023-11-30 21:50:33 -0300

    últimos exercícios

---
+++ provas/Condicionais/verificar_triangulos.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+int lado1, lado2, lado3;
+
+scanf("%d", &lado1);
+scanf("%d", &lado2);
+scanf("%d", &lado3);
+
+if(lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado2 + lado1){
+printf("possivel");
+}
+else{
+printf("impossivel");
+}
+return 0;
+}
